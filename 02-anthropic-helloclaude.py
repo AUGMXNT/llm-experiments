5f377b1e5aabd97fc98e03814899167d7b003916
lhl <lhl@randomfoo.net>
2023-04-18 03:25:12 +0900

    better hello world prompt

--- 02-anthropic-helloclaude.py
+++ 02-anthropic-helloclaude.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Examples here: https://github.com/anthropics/anthropic-sdk-python/tree/main/examples
 
 import asyncio
@@ -8,7 +10,7 @@ import anthropic
 async def main(max_tokens_to_sample: int = 100):
     c = anthropic.Client(os.environ["ANTHROPIC_API_KEY"])
     resp = await c.acompletion(
-        prompt=f"{anthropic.HUMAN_PROMPT} How many toes do dogs have?{anthropic.AI_PROMPT}",
+        prompt=f"{anthropic.HUMAN_PROMPT} Please tell me about yourself.{anthropic.AI_PROMPT}",
         stop_sequences=[anthropic.HUMAN_PROMPT],
         model="claude-v1",
         max_tokens_to_sample=max_tokens_to_sample,
